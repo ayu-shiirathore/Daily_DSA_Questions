da7831b25c83a37292312c617b1537543079846c
Ayushii <139373709+ayu-shiirathore@users.noreply.github.com>
2025-01-20 15:52:36 +0530

    Time: 8 ms (87.2%), Space: 134 MB (82.62%) - LeetHub

---
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        int size = m*n;
+
+        // step1 : create a position vector for each element of the matrix
+        vector<pair<int,int>> pos(size+1);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++)
+            pos[mat[i][j]]=make_pair(i,j);
+        }      
+
+        //find earliest row or column painted
+        vector<int> rowcount(m,0);
+        vector<int> colcount(n,0);
+        for(int i=0;i<size;i++){
+            pair<int,int> coordinate = pos[arr[i]];
+            int x= coordinate.first;
+            int y= coordinate.second;
+            rowcount[x]++;
+            colcount[y]++;
+            if(rowcount[x]==n || colcount[y]==m)
+            return i;
+        }
+        return -1;
+    }
+};
