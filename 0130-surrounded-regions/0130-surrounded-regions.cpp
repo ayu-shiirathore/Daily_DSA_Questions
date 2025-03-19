acfa70d95d067b4cd720ce8ba527c45660b27283
Ayushii <139373709+ayu-shiirathore@users.noreply.github.com>
2025-03-19 13:00:53 +0530

    Time: 1 ms (35.96%), Space: 14.5 MB (48.95%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,16 +1,18 @@
 class Solution {
 private:
-void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>>& mat, int delrows[], int delcols[]){
+void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>>& mat){
     vis[row][col]=1;
     int n=mat.size();
     int m= mat[0].size();
+    int delrows[] = {-1,0,1,0};
+    int delcols[] = {0,1,0,-1};
 
     //check for top,right,botton,left
     for(int i=0;i<4;i++){
         int nrow = row + delrows[i];
         int ncol = col + delcols[i];
         if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && mat[nrow][ncol]=='O'){
-            dfs(nrow, ncol, vis, mat, delrows, delcols);
+            dfs(nrow, ncol, vis, mat);
         }
     }
 }
@@ -19,18 +21,16 @@ public:
         int n = mat.size();
         int m = mat[0].size();
         vector<vector<int>> vis(n,vector<int>(m,0));
-        int delrows[] = {-1,0,1,0};
-        int delcols[] = {0,1,0,-1};
 
         //traverse first and last row and perform dfs on boundary 0s
         for(int j=0;j<m;j++){
             //first row
             if(!vis[0][j] && mat[0][j]=='O'){
-                dfs(0,j,vis,mat, delrows, delcols);
+                dfs(0,j,vis,mat);
             }
             //last row
             if(!vis[n-1][j] && mat[n-1][j]=='O'){
-                dfs(n-1,j,vis,mat, delrows, delcols);
+                dfs(n-1,j,vis,mat);
             }
         }
 
@@ -38,11 +38,11 @@ public:
         for(int i=0;i<n;i++){
             //first column
             if(!vis[i][0] && mat[i][0]=='O'){
-                dfs(i,0,vis,mat, delrows, delcols);
+                dfs(i,0,vis,mat);
             }
             //last column
             if(!vis[i][m-1] && mat[i][m-1]=='O'){
-                dfs(i,m-1,vis,mat, delrows, delcols);
+                dfs(i,m-1,vis,mat);
             }
         }
 
