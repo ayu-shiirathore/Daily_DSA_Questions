b3881c76abcbf68b9ad82ffd94b6aa038e287e13
Ayushii <139373709+ayu-shiirathore@users.noreply.github.com>
2025-03-20 20:59:00 +0530

    Time: 45 ms (54.85%), Space: 66.3 MB (33.28%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<bool> visit(n), inStack(n);
-        for (int i = 0; i < n; i++) {
-            dfs(i, graph, visit, inStack);
-        }
-        vector<int> safeNodes;
-        for (int i = 0; i < n; i++) {
-            if (!inStack[i]) {
-                safeNodes.push_back(i);
-            }
-        }
-        return safeNodes;
-    }
+        int V = graph.size();
 
-    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visit,
-             vector<bool>& inStack) {
-        if (inStack[node]) {
-            return true;
-        }
-        if (visit[node]) {
-            return false;
-        }
-        visit[node] = true;
-        inStack[node] = true;
-        for (auto neighbor : adj[node]) {
-            if (dfs(neighbor, adj, visit, inStack)) {
-                return true;
+        // Create reverse adjacency list and indegree array
+        vector<vector<int>> adjRev(V);
+        vector<int> indegree(V, 0);
+    
+        // Step 1: Build the reversed graph and calculate indegree
+        for (int i = 0; i < V; ++i) {
+            for (int neighbor : graph[i]) {
+                adjRev[neighbor].push_back(i);
+                indegree[i]++;
             }
         }
-        inStack[node] = false;
-        return false;
+
+        queue<int> q;
+        vector<int> safe;
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            safe.push_back(node);
+
+            for (auto it : adjRev[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+
+        sort(safe.begin(), safe.end());
+
+        return safe;
     }
 };
