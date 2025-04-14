ffb1549f12b20cf97295b8d44f5520371db61842
Ayushii <139373709+ayu-shiirathore@users.noreply.github.com>
2025-04-14 13:27:02 +0530

    Time: 19 ms (31.71%), Space: 14.5 MB (15.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,25 +3,17 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
         int left = 0, right = 0, len = 0;
-        string st = "";
+        unordered_set<char> st;
 
         while (right < n) {
-            char c = s[right];
-
-            // If the character is already in the current substring
-            // st.find(c) != std::string::npos this condition checks if 
-            // the character is already present
-            if (st.find(c) != std::string::npos) {
-                // Remove characters from the left until we remove the duplicate
-                while (st.find(c) != std::string::npos) {
-                    st.erase(0, 1);  // Remove the first character
-                    left++;
-                }
+            if (st.find(s[right]) == st.end()) {
+                st.insert(s[right]);
+                len = max(len, right - left + 1);
+                right++;
+            } else {
+                st.erase(s[left]);
+                left++;
             }
-
-            st += c;
-            len = max(len, right - left + 1);
-            right++;
         }
 
         return len;
