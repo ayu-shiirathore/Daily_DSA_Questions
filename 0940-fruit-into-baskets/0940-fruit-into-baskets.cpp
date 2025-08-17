5d04cf62127ba7895970de8dfeff6071e07641a2
Ayushii <139373709+ayu-shiirathore@users.noreply.github.com>
2025-08-17 18:00:44 +0530

    Time: 44 ms (38.16%), Space: 74.9 MB (73.11%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> fruitCount; // To store the frequency of each fruit type in the current window
-        int left = 0;                       
-        int maxFruits = 0;                  
+        unordered_map<int,int>mp;
+        int r=0;
+        int l=0;
+        int maxLen=0;
 
-        for (int right = 0; right < fruits.size(); ++right) {
-            // Add the current fruit to the window
-            fruitCount[fruits[right]]++;
-
-            // If the window has more than two types of fruits, we need to shrink it from the left
-            while (fruitCount.size() > 2) {
-                fruitCount[fruits[left]]--;        // Decrement the count of the leftmost fruit
-                if (fruitCount[fruits[left]] == 0) {
-                    fruitCount.erase(fruits[left]); // Remove the fruit type if its count drops to zero
-                }
-                left++;                            // Move the left pointer to the right
+        while(r<fruits.size()){    
+            mp[fruits[r]]++;
+            while(mp.size()>2){
+                mp[fruits[l]]--;
+                if(mp[fruits[l]]==0) mp.erase(fruits[l]);
+                l++;
             }
-
-            // Update the maximum number of fruits picked so far
-            maxFruits = max(maxFruits, right - left + 1);
+            maxLen=max(maxLen,r-l+1);
+            r++;
         }
-
-        return maxFruits;
+        return maxLen;
     }
 };
