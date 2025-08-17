c8857d3eb0d49227d13eb6eeba2721127ff56753
Ayushii <139373709+ayu-shiirathore@users.noreply.github.com>
2025-08-17 18:00:58 +0530

    Time: 47 ms (29.01%), Space: 75.1 MB (53.75%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -8,7 +8,7 @@ public:
 
         while(r<fruits.size()){    
             mp[fruits[r]]++;
-            while(mp.size()>2){
+            if(mp.size()>2){
                 mp[fruits[l]]--;
                 if(mp[fruits[l]]==0) mp.erase(fruits[l]);
                 l++;
