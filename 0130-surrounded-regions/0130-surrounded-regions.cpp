988847f6ec9522b99c406e5478f05b554dfd3f77
Ayushii <139373709+ayu-shiirathore@users.noreply.github.com>
2025-08-30 15:03:46 +0530

    Time: 0 ms (100%), Space: 14.6 MB (38.63%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,56 +1,57 @@
 class Solution {
-private:
-void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>>& mat){
-    vis[row][col]=1;
-    int n=mat.size();
-    int m= mat[0].size();
-    int delrows[] = {-1,0,1,0};
-    int delcols[] = {0,1,0,-1};
+public:
+    void dfs(int row, int col, vector<vector<char>>& board, vector<vector<int>> &vis, int delrow[], int delcol[]){
+        vis[row][col] = 1;
+        int m = board.size();
+        int n = board[0].size();
 
-    //check for top,right,botton,left
-    for(int i=0;i<4;i++){
-        int nrow = row + delrows[i];
-        int ncol = col + delcols[i];
-        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && mat[nrow][ncol]=='O'){
-            dfs(nrow, ncol, vis, mat);
+        for(int i=0; i<4; i++){
+            int nrow = row+delrow[i];
+            int ncol = col+delcol[i];
+
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol] && board[nrow][ncol]=='O'){
+                dfs(nrow, ncol, board, vis, delrow, delcol);
+            }
         }
     }
-}
-public:
-    void solve(vector<vector<char>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
 
-        //traverse first and last row and perform dfs on boundary 0s
-        for(int j=0;j<m;j++){
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+
+        vector<vector<int>> vis(m, vector<int>(n,0));
+
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+
+        //travesrse first and last row
+        for(int j=0; j<n; j++){
             //first row
-            if(!vis[0][j] && mat[0][j]=='O'){
-                dfs(0,j,vis,mat);
+            if(!vis[0][j] && board[0][j]=='O'){
+                dfs(0,j,board,vis,delrow,delcol);
             }
             //last row
-            if(!vis[n-1][j] && mat[n-1][j]=='O'){
-                dfs(n-1,j,vis,mat);
+            if(!vis[m-1][j] && board[m-1][j]=='O'){
+                dfs(m-1,j,board,vis,delrow,delcol);
             }
         }
 
-        //traverse first and last column and perform dfs on boundary 0s
-        for(int i=0;i<n;i++){
-            //first column
-            if(!vis[i][0] && mat[i][0]=='O'){
-                dfs(i,0,vis,mat);
+        //traverse first and last col
+        for(int i=0; i<m; i++){
+            //first col
+            if(!vis[i][0] && board[i][0]=='O'){
+                dfs(i,0,board,vis,delrow,delcol);
             }
-            //last column
-            if(!vis[i][m-1] && mat[i][m-1]=='O'){
-                dfs(i,m-1,vis,mat);
+            //last col
+            if(!vis[i][n-1] && board[i][n-1]=='O'){
+                dfs(i,n-1,board,vis,delrow,delcol);
             }
         }
 
-        //traverse whole matrix for remaining zeros and convert them to X
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j])
-                mat[i][j]='X';
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!vis[i][j] && board[i][j] == 'O')
+                    board[i][j]='X';
             }
         }
     }
